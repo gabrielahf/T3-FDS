cdfb8f1c87d78b745d50d370bd494bf919c68463
gabrielahf <127217373+gabrielahf@users.noreply.github.com>
2023-11-07 10:54:00 -0300

    relogio_xadrez.vhd

--- relogio_xadrez.vhd
+++ relogio_xadrez.vhd
@@ -10,10 +10,10 @@ use ieee.std_logic_unsigned.all;
 library work;
 
 entity relogio_xadrez is
-    port( j1,j2,load,clock,reset: in std_logic;
+    port( load,clock,reset: in std_logic;
           init_time: in std_logic_vector (7 downto 0);
-          winJ1,winJ2: out std_logic
-	  --contj1,contj2: out std_logic_vector (15 downto 0)
+          winJ1,winJ2: out std_logic;
+	  contj1,contj2: out std_logic_vector (15 downto 0)
 	);
           
 
@@ -25,15 +25,15 @@ architecture relogio_xadrez of relogio_xadrez is
     -- DECLARACAO DOS ESTADOS
     type states is ( INICIO, TIME0, START,j1 ,j2, VIT1, VIT2);
     signal EA, PE : states;
-    signal J1, J2, load_interno, en1, en2: std_logic;
-    signal contj1,contj2 : std_logic_vector (15 downto 0);
+    signal load_interno, en1, en2: std_logic;
+    signal cont1,cont2 : std_logic_vector (15 downto 0);
     -- ADICIONE AQUI OS SINAIS INTERNOS NECESSARIOS
     
 begin
 
     -- INSTANCIACAO DOS CONTADORES
-    contador1 : entity work.temporizador port map ( clock => clock, reset => reset, load => load_interno, en => en1, init_time => init_time, cont => contj1);
-    contador2 : entity work.temporizador port map ( clock => clock, reset => reset, load => load_interno, en => en2, init_time => init_time, cont => contj2);
+    contador1 : entity work.temporizador port map ( clock => clock, reset => reset, load => load_interno, en => en1, init_time => init_time, cont => cont1);
+    contador2 : entity work.temporizador port map ( clock => clock, reset => reset, load => load_interno, en => en2, init_time => init_time, cont => cont2);
 
     -- PROCESSO DE TROCA DE ESTADOS
     process (clock, reset)
@@ -49,7 +49,7 @@ begin
     end process;
 
     -- PROCESSO PARA DEFINIR O PROXIMO ESTADO
-    process (EA,J1,J2,contj1,contj2) --<<< Nao esqueca de adicionar os sinais da lista de sensitividade
+    process (EA,en1,en2,cont1,cont2) --<<< Nao esqueca de adicionar os sinais da lista de sensitividade
 begin
     case EA is
         when INICIO =>
@@ -59,26 +59,26 @@ begin
         when TIME0 =>
             PE <= START;
         when START =>
-            if J1 = '1' then 
+            if en1 = '1' then 
                 PE <= j1;
-            elsif J2 = '1' then
+            elsif en2 = '1' then
                 PE <= j2;
             else PE <= START;
             end if;
         when j1 =>
-            if J1 = '1' then 
+            if en1 = '1' then 
                 PE <= j2;
-            elsif J1 = '0' then
+            elsif en1 = '0' then
                 PE <= j1;
-            elsif contj1 = '000000000000000' then
+            elsif cont1 = '000000000000000' then
                 PE <= VIT2;
             end if;
         when j2 =>
-            if J2 = '1' then 
+            if en2 = '1' then 
                 PE <= j1;
-            elsif J2 = '0' then
+            elsif en2 = '0' then
                 PE <= j2;
-            elsif contj2 = '000000000000000' then
+            elsif cont2 = '000000000000000' then
                 PE <= VIT1;
             end if;
         when VIT1 =>
@@ -94,6 +94,8 @@ end process;
     j1 <= '1' when EA = en1 else '0';
     j2 <= '1' when EA = en2 else '0';
     load_interno <= '1' when EA = TIME0 else '0';
+    contj1 <= '1' when EA = cont1 else '0';
+    contj2 <= '1' when EA = cont2 else '0';
 
 end relogio_xadrez;
 
